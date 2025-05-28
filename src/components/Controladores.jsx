ef8872985c58dc880d32ddeffe52377025737724
Lenin Rojas Sánchez <leninrojassanchez09@gmail.com>
2025-05-27 23:12:32 -0600

    Proyecto primera versión

--- src/components/Controladores.jsx
+++ src/components/Controladores.jsx
@@ -4,17 +4,25 @@ import { faXTwitter } from '@fortawesome/free-brands-svg-icons'; // Twitter como
 import DownloadIcon from '@mui/icons-material/Download';
 import '../Controladores.css'
 
+function Controladores(props) {
+  function handleClick() {
+    props.actualizar();
+    //console.log(props.actualizar)
+  }
 
-function Controladores() {
+  function descargar() {
+    props.descargar();
+  }
+
   return (
     <div className="controladores">
-        <button id="new-quote">Nueva frase</button>
+        <button id="new-quote" onClick={handleClick}>Nueva frase</button>
       
       <div className="mini-botones-contenedor">
         <a id="tweet-quote" href="https://twitter.com/intent/tweet?" target="_top" className="mini-botones mini-twitter">
           <FontAwesomeIcon icon={faXTwitter} className="twitter-icon"/>
         </a>
-        <button className="mini-botones mini-descarga">
+        <button className="mini-botones mini-descarga" onClick={descargar}>
           <DownloadIcon className="download-icon" />
         </button>
       </div>
--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import cristianCara from '../assets/cris.png'
+import nancyCara from '../assets/nancy.png'
+import isakCara from '../assets/isak.png'
 import Controladores from './Controladores';
 import '../Frase.css'
+import html2canvas from 'html2canvas';
 
 
 function aleatorio (min, max) {
@@ -13,31 +16,76 @@ const frases = [
     frase: '"Jimi vs Kimi: Dawn of monkeys".',
     autor: "Cristian Tutub",
     imagen: cristianCara,
-    dijo: 'siempre'
+    complemento: 'siempre'
   },
   {
-    frase: '"Jimi vs Kimi: Dawn of monkeys".',
-    autor: "Cristian Tutub",
-    imagen: cristianCara,
-    dijo: 'nunca'
+    frase: '"Mmmmmmm".',
+    autor: "Nancy Amascar",
+    imagen: nancyCara,
+    complemento: 'nunca'
+  }, 
+  {
+    frase: '"¿Qué hay de mí? ¿Qué hay de lo que quiero? ¿Qué hay de lo que siento?".',
+    autor: "Pineapple Isak",
+    imagen: isakCara,
+    complemento: 'nunca'
   }
 ];
 
-let fraseInicial = frases[aleatorio(0, frases.length - 1)];
+let index = aleatorio(0, frases.length - 1);
+let fraseActual = frases[index];
 
 function Frase() {
+  const [imagen, setImagen] = useState(fraseActual.imagen);
+  const [frase, setFrase] = useState(fraseActual.frase);
+  const [autor, setAutor] = useState(fraseActual.autor);
+  const [complemento, setComplemento] = useState(fraseActual.complemento);
+
+  function actualizarFrase() {
+    let nuevaLista = [...frases];
+    nuevaLista.splice(index, 1);
+    let randomIndex = aleatorio(0, nuevaLista.length - 1);
+    let nuevaFrase = nuevaLista[randomIndex];
+
+
+    setImagen(nuevaFrase.imagen);
+    setFrase(nuevaFrase.frase);
+    setAutor(nuevaFrase.autor);
+    setComplemento(nuevaFrase.complemento);
+
+    frases.forEach((e, i)=> {
+      if (nuevaFrase.frase == e.frase) {
+        fraseActual = frases[i];
+        index = i;
+      }
+    })
+  }
+
+  function descargarImagen() {
+    html2canvas(document.querySelector(".up"), {
+      backgroundColor: '#000',
+      color: '#fff'
+    }).then(canvas => {
+    const enlace = document.createElement('a');
+    enlace.download = 'frase.jpg';
+    enlace.href = canvas.toDataURL('image/jpeg'); // convierte el canvas en una imagen base64
+    enlace.click();
+});
+
+  }
+
   return (
     <article id="quote-box">
       <section className="up">
         <div className='imagen-contenedor'>
-          <img src={fraseInicial.imagen}/>
+          <img src={imagen} className="caritas"/>
         </div>
         <div className="textos">
-          <p id="text">{fraseInicial.frase}</p>
-          <p id="author">-Frases que {fraseInicial.autor} {fraseInicial.dijo} dijo</p>
+          <p id="text">{frase}</p>
+          <p id="author">-Frases que {autor} {complemento} dijo</p>
         </div>
       </section>
-      <Controladores />
+      <Controladores actualizar={actualizarFrase} descargar={descargarImagen}/>
     </article>
   )
 }
